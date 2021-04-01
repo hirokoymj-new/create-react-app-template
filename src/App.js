c9610a778e883d1109029add7bd56ad18ce5e21f
Hiroko Yamaji <hiroko@hirokoymj.com>
2021-04-01 11:59:45 +0900

    added node version in package.json

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
-import config from "Config/config";
 
 const App = () => {
   console.log("NODE_ENV:", process.env.NODE_ENV);
-  console.log("REACT_APP_LOCAL:", process.env.REACT_APP_LOCAL);
-  console.log(JSON.stringify(config));
+  console.log("REACT_APP_STAGE:", process.env.REACT_APP_STAGE);
 
   return (
     <div className="App">
       Hello World
       <h1>{process.env.NODE_ENV}</h1>
-      <h1>{process.env.REACT_APP_LOCAL}</h1>
+      <h1>{process.env.REACT_APP_STAGE}</h1>
     </div>
   );
 };
